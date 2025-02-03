40881a7b2ea09a6a3e10e6abe585fc3eb5f38e19
Lakshay-frontend-Dev <vermaff495@gmail.com>
2025-02-03 16:04:43 +0530

    update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`📦 MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("📦 MongoDB Connected Successfully!");
   } catch (error) {
     console.error(`❌ Error: ${error.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
